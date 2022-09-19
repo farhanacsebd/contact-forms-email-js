b38f1f0946fbde960329ed61ca3f23c9a52eda70
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-19 23:11:47 +0600

    email js add

--- main/app.js
+++ main/app.js
@@ -1,1 +1,31 @@
-console.log('hello');
+function sendEmail(){
+
+    (function(){
+        emailjs.init("user_48v0v2ixexu0gGtiBREac");
+     })();  
+
+let name = document.getElementById('name').value;
+let email = document.getElementById('email').value;
+let phone = document.getElementById('phone').value;
+let subject = document.getElementById('subject').value;
+let message = document.getElementById('message').value;
+
+
+var templateParams = {
+
+    firstName : name,
+    myEmail : email,
+    myPhone : phone,
+    mySubject : subject,
+    myMessage : message,
+
+};
+
+emailjs.send('catseller', 'catsellerId', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+}
+ 
